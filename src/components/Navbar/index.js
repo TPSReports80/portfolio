cecb94b3cc4eda689f0215f9c02e82dcaa2200ba
TPSReports80 <mikejoo80@gmail.com>
2022-11-26 02:39:33 -0800

    updated resume

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { CgMenu } from "react-icons/cg";
 import { useSpring, animated } from "react-spring";
-import Resume from "../../static/Michael_Joo.pdf";
+import Resume from "../../static/Michael_Joo_Resume.pdf";
 import {
   Nav,
   NavbarContainer,
