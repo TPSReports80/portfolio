9c9a36b0dadca59b7bd47a63b11b3f93ba47935d
TPSReports80 <mikejoo80@gmail.com>
2023-01-24 00:36:17 -0800

    updated work component

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -57,9 +57,9 @@ const Work = () => {
             <JobTitle>Career Break</JobTitle>
             <JobLength>Sep 2020 - Present</JobLength>
             <JobDesc>
-              I took some time-off due to personal reasons. I also made use of
-              this period to strengthen my knowledge and be kept up-to-date with
-              current technologies.
+              I took time-off and a break from my career due to the pandemic and
+              for personal reasons. I also took advantage of this time to keep
+              up to date with current technologies.
             </JobDesc>
           </TabsContent>
           <TabsContent
